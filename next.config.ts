37e7167381ce14646935a6b173d1ab28654b099a
M Hacker <hacker@bfsio.net>
2025-11-28 19:52:30 -0600

    chore: move typedRoutes configuration to the correct position in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  typedRoutes: true,
   experimental: {
-    typedRoutes: true,
     optimizePackageImports: [
       'react',
       'react-dom'
